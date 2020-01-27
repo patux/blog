0304d01f7808b0a5fa88a5869e339c8a446acdb1
Geronimo Orozco <gorozco@gmail.com>
2020-01-26 22:57:00 -0600

    hugo.sh: fix add params

--- hugo.sh
+++ hugo.sh
@@ -4,6 +4,6 @@ case "$1" in
         docker run --user `id -u` --rm  -p 1313:1313 -v $(pwd):/usr/share/blog monachus/hugo hugo -v server --bind=0.0.0.0
         ;;
     *)
-        docker run --user `id -u` --rm  -v $(pwd):/usr/share/blog monachus/hugo hugo -v
+        docker run --user `id -u` --rm  -v $(pwd):/usr/share/blog monachus/hugo hugo -v $@
         ;;
 esac
