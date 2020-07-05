569800337dcbfb8489473fd5ebb175c6890ef4d8
Geronimo Orozco <gorozco@gmail.com>
2020-07-05 15:40:44 -0500

    hugo.sh fix script to lauch properly when server param is used

--- hugo.sh
+++ hugo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# set -v
 docker pull monachus/hugo
 case "$1" in
     "server")
-        docker run --runtime runc --user `id -u` --rm  -p 1313:1313 -v $(pwd):/usr/share/blog monachus/hugo hugo -v server --bind=0.0.0.0
+        docker run --runtime runc --user `id -u` --rm  -p 1313:1313 -v $(pwd):/usr/share/blog monachus/hugo hugo -v $@ --bind=0.0.0.0
         ;;
     *)
         docker run --runtime runc --user `id -u` --rm  -v $(pwd):/usr/share/blog monachus/hugo hugo -v $@
