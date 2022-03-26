f3c2d64283b1e8506755fc9bc28f8fd81352af29
Geronimo Orozco <gorozco@gmail.com>
2022-03-26 01:00:41 -0600

    hugo.sh: set datetime of host

--- hugo.sh
+++ hugo.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # set -v
 docker pull monachus/hugo
+
 case "$1" in
     "server")
-        docker run --runtime runc --user `id -u` --rm  -p 1313:1313 -v $(pwd):/usr/share/blog monachus/hugo hugo -v $@ --bind=0.0.0.0
+        docker run --runtime runc --user `id -u` --rm  -p 1313:1313 -v $(pwd):/usr/share/blog -v /etc/timezone:/etc/timezone -v /etc/localtime:/etc/localtime  monachus/hugo hugo -v $@ --bind=0.0.0.0
         ;;
     *)
-        docker run --runtime runc --user `id -u` --rm  -v $(pwd):/usr/share/blog monachus/hugo hugo -v $@
+        docker run --runtime runc --user `id -u` --rm  -v $(pwd):/usr/share/blog -v /etc/timezone:/etc/timezone -v /etc/localtime:/etc/localtime  monachus/hugo hugo -v $@
         ;;
 esac
